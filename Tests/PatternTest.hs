8896bc411898a018b76afb9a6c960061d2e08694
Reinert L <reilemx@gmail.com>
2019-11-26 17:00:42 +0100

    Test tweak

--- Tests/PatternTest.hs
+++ Tests/PatternTest.hs
@@ -3,7 +3,7 @@ data Number = Zero | Succ Number
 data List (a :: *) = Nil | Cons a (List a)
 
 -- Regular patterns test
-\x. let length = \y. case (Cons Zero y) of
+\x. let length = \y. case y of
     Nil       -> Zero
     Cons x xs -> Succ (length xs)
   in
