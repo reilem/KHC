5c109fdbf867ceca50a9474262b3f805226be48a
Reinert L <reilemx@gmail.com>
2020-02-12 12:05:46 +0100

    Add initial Or pattern test

---
+++ Tests/OrPatternTest.hs
@@ -0,0 +1,6 @@
+data Tuple (a :: *) (b :: *) (c :: *) (d :: *) = Double a b | Triple a b c | Quad a b c d
+
+-- Test uses type constructors with more than two data contructors with
+-- more than 2 parameters. Contains incomplete patterns.
+\x. case x of
+  Double x y || Triple x y _ -> x
