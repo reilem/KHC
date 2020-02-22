45ec5e26873c9e199458e47bef8df8c05585eedd
Reinert L <reilemx@gmail.com>
2020-02-22 16:11:19 +0100

    Update test descriptions

--- Tests/OrPatternTest.hs
+++ Tests/OrPatternTest.hs
@@ -1,6 +1,5 @@
 data Tuple (a :: *) (b :: *) (c :: *) (d :: *) = Double a b | Triple a b c | Quad a b c d
 
--- Test uses type constructors with more than two data contructors with
--- more than 2 parameters. Contains incomplete patterns.
+-- Tests a basic usage of OR patterns.
 \x. case x of
   Double x y || Triple x y _ -> x
