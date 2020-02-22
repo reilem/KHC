9c6a03ea7e9bb1be4b89b159683d90c0a4dc9bf9
Reinert L <reilemx@gmail.com>
2020-02-22 16:12:57 +0100

    Update test description

--- Tests/OrPatternTestFlip.hs
+++ Tests/OrPatternTestFlip.hs
@@ -1,6 +1,6 @@
 data Tuple (a :: *) (b :: *) (c :: *) (d :: *) = Double a b | Triple a b c | Quad a b c d
 
--- Test uses type constructors with more than two data contructors with
--- more than 2 parameters. Contains incomplete patterns.
+-- Tests a basic usage of OR patterns. Differs from OrPatternTest because
+-- the variables x y are now flipped in the right branch.
 \i. case i of
   Double x y || Triple y x _ -> x
