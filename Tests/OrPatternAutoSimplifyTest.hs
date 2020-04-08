0aa99ea750a9de7d6d55bd445b67417b4d8435e9
Reinert L <reilemx@gmail.com>
2020-04-08 14:41:49 +0200

    Cleanup tests and update test script

--- Tests/OrPatternAutoSimplifyTest.hs
+++ Tests/OrPatternAutoSimplifyTest.hs
@@ -4,5 +4,5 @@ data Number = Zero | Succ Number
 -- Tests a usage of OR patterns. The compiler should output a simplified
 -- versions because xs subsumes (Cons _ xs).
 \x. case x of
-  Nil                                           -> Cons (Cons Zero Nil) Nil
-  Cons (Succ x)  || Cons (Succ x) (Cons _ xs) -> Cons (Cons )
+  Nil                                         -> Cons (Cons Zero Nil) Nil
+  Cons (Succ x) xs || Cons (Succ x) (Cons _ xs) -> Cons (Cons x xs) Nil
