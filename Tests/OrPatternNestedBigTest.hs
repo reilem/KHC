c7fae177d0b0f33916fcc2500308a5fb4b0ba33f
Reinert L <reilemx@gmail.com>
2020-04-13 15:23:47 +0200

    Improve OrPatternNestedBigTest

--- Tests/OrPatternNestedBigTest.hs
+++ Tests/OrPatternNestedBigTest.hs
@@ -1,6 +1,7 @@
 data Tuple (a :: *) (b :: *) (c :: *) (d :: *) = Double a b | Triple a b c | Quad a b c d
+data Number = Zero | Succ Number
 
 -- Tests a nested usage of OR patterns. Should succeed.
 \i. case i of
-  Double (Triple x _ _ || Quad x _ _ _) y -> x
-  Double (Double _ x || Triple _ x _) y -> x
+  Double (Triple x _ _ || Quad x _ _ _) (Succ y) -> x
+  Double (Double _ x || Triple _ x _) Zero -> x
