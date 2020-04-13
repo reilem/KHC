b118e412134b2a6a262a22f365b90a089b258f60
Reinert L <reilemx@gmail.com>
2020-04-13 15:49:27 +0200

    Improve documentation of good tests

--- Tests/Guards/BigWithOrTest.hs
+++ Tests/Guards/BigWithOrTest.hs
@@ -2,7 +2,8 @@ data Number = Zero | Succ Number
 data Tuple (a :: *) (b :: *) (c :: *) (d :: *) = Double a b | Triple a b c | Quad a b c d
 data List (a :: *) = Nil | Cons a (List a)
 
--- Test has guard patterns and includes an OR pattern
+-- Test has an or-pattern that has guards in the rhs
+-- GOOD TEST: SHOWS BIG SIZE DIFFERENCE BETWEEN FLATTENING AND LET-BINDINGS OF OR-PATTERNS
 \x. case x of
     Cons x _ || Cons _ (Cons x Nil)
       | Double _ a <- x, Double b _   <- a -> Succ b
--- Tests/OrPatternNestedBigTest.hs
+++ Tests/OrPatternNestedBigTest.hs
@@ -2,6 +2,8 @@ data Tuple (a :: *) (b :: *) (c :: *) (d :: *) = Double a b | Triple a b c | Qua
 data Number = Zero | Succ Number
 
 -- Tests a nested usage of OR patterns. Should succeed.
+-- GOOD TEST: SHOWS DIFFERENCE BETWEEN OPTIMISED AND UNOPTIMISED COMPILATION OF
+-- OR-PATTERNS USING LET-BINDINGS
 \i. case i of
   Double (Triple x _ _ || Quad x _ _ _) (Succ y) -> x
   Double (Double _ x || Triple _ x _) Zero -> x
