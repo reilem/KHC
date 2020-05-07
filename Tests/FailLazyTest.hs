a5dcbe5bc876c348a19fbeb3d493d4540ea48fdb
Reinert L <reilemx@gmail.com>
2020-05-07 18:49:34 +0200

    Add two other interesting examples for paper

--- Tests/FailLazyTest.hs
+++ Tests/FailLazyTest.hs
@@ -1,8 +1,8 @@
 data Number = Zero | Succ Number
 data List (a :: *) = Nil | Cons a (List a)
 
--- Test has lazy pattern matching with an or and guards with will fail
-case (Cons (error "head error") Nil) of
+-- Test has lazy pattern matching with an or and guards which will fail
+case (Cons (Succ (error "head error")) Nil) of
   Cons x xs
     | (Succ Zero || Zero) <- x -> Succ Zero
     | (Succ y)            <- x -> y
