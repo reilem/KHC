35d1940e735c742d6f1f9e7c7150f6258f076ed3
Reinert L <reilemx@gmail.com>
2020-05-07 18:12:35 +0200

    Lazy tests doc fixes

--- Tests/FailLazyTest.hs
+++ Tests/FailLazyTest.hs
@@ -1,7 +1,7 @@
 data Number = Zero | Succ Number
 data List (a :: *) = Nil | Cons a (List a)
 
--- Test has nested Patterns
+-- Test has lazy pattern matching with an or and guards with will fail
 case (Cons (error "head error") Nil) of
   Cons x xs
     | (Succ Zero || Zero) <- x -> Succ Zero
--- Tests/LazyMatchTest.hs
+++ Tests/LazyMatchTest.hs
@@ -1,7 +1,7 @@
 data Number = Zero | Succ Number
 data List (a :: *) = Nil | Cons a (List a)
 
--- Test has nested Patterns
+-- Test has lazy pattern matching with an or and guards with will succeed
 case (Cons (Succ Zero) (error "tail error")) of
   Cons x xs
     | (Succ Zero || Zero) <- x -> Succ Zero
