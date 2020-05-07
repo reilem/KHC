2fb7879324fad42e0d945a6fe3d6f642919c964e
Reinert L <reilemx@gmail.com>
2020-05-07 19:50:02 +0200

    Move lazy tests to guards folder and fix docs

--- Tests/Guards/LazyMatchTest.hs
+++ Tests/Guards/LazyMatchTest.hs
@@ -1,8 +1,10 @@
 data Number = Zero | Succ Number
 data List (a :: *) = Nil | Cons a (List a)
 
--- Test has lazy pattern matching with an or and guards which will fail
-case (Cons (Succ (error "head error")) Nil) of
+-- TEST USED IN PAPER
+-- Test has or-patterns in pattern guards and a diverging input
+-- Test should succeed with: Succ Zero
+case (Cons (Succ Zero) (error "tail error")) of
   Cons x xs
     | (Succ Zero || Zero) <- x -> Succ Zero
     | (Succ y)            <- x -> y
