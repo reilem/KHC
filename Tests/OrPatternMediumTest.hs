61cdb800f99c655e4273412461e3d4f9fd41dcf4
Reinert L <reilemx@gmail.com>
2020-05-09 19:00:00 +0200

    Fix medium test

--- Tests/OrPatternMediumTest.hs
+++ Tests/OrPatternMediumTest.hs
@@ -4,6 +4,7 @@ data Number = Zero | Succ Number
 -- Tests a more complex usage of OR patterns.
 -- Should return: Cons (Cons (Succ Zero) Nil) Nil
 (\a. case a of
-  Cons (Succ x) xs || Cons Zero (Cons x xs) -> Cons (Cons x xs) Nil
-  Nil                                       -> Cons Nil Nil
+  Cons (Succ Zero || Succ (Succ Zero) || Zero) xs -> Cons (Succ Zero) xs
+  Cons x                                       xs -> Cons x Nil
+  Nil                                             -> Cons Zero Nil
 ) (Cons Zero (Cons (Succ Zero) Nil))
